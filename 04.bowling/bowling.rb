3189eb8fedb43f8c51758666b310bebea1e3beba
Kakutani Shintaro <shintaro@kakutani.com>
2021-02-18 12:11:32 +0900

    :police_car: rubocop -A

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+STRIKE = 10
 def parse_marks(marks)
-  marks
+  marks.split(',').map { |m| m == 'X' ? STRIKE : m.to_i }
 end
--- 04.bowling/bowling_test.rb
+++ 04.bowling/bowling_test.rb
@@ -7,7 +7,7 @@ require_relative 'bowling'
 class BowlingTest < Minitest::Test
   def test_parse_marks
     input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    expect = [[6,3],[9,0],[0,3],[8,2],[7,3],[10, nil],[9,1],[8,0],[10, nil],[6,4,5]]
+    expect = [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10, nil], [9, 1], [8, 0], [10, nil], [6, 4, 5]]
     assert_equal expect, parse_marks(input)
   end
 end

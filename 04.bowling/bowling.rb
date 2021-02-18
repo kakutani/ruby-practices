07ca67c374639ad99fea761c8428a802862501c6
Kakutani Shintaro <shintaro@kakutani.com>
2021-02-18 12:17:58 +0900

    :green_circle: #parse_marks

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -3,5 +3,16 @@
 
 STRIKE = 10
 def parse_marks(marks)
-  marks.split(',').map { |m| m == 'X' ? STRIKE : m.to_i }
+  pins = marks.split(',').map { |m| m == 'X' ? STRIKE : m.to_i }
+  frames = []
+  9.times do
+    rolls = pins.shift(2)
+    if rolls.first == STRIKE
+      frames << [rolls.first, nil] # 2投目は投げていない
+      pins.unshift(rolls.last) # 取りすぎなので戻す
+    else
+      frames << rolls
+    end
+  end
+  frames << pins # 最後に残ったのが10フレーム目
 end
